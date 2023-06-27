eb62b5ab829003bf334f6cfb962db611ff386afd
vitalspace <myglobalsounds@gmail.com>
2023-06-26 21:51:22 -0600

    init project

---
+++ src/App.svelte
@@ -0,0 +1,111 @@
+<div class="flex h-screen items-center justify-center bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-slate-300 via-lime-50 to-rose-700">
+  <div class="w-full space-y-4 rounded-xl bg-red-50 px-4 py-4 font-bold text-gray-600 md:w-2/5">
+    <div class="flex items-center justify-between px-2">
+      <h2 class="text-3xl  pl-1">Calendar</h2>
+      <button class="rounded-lg bg-red-400 px-2 py-2 text-white shadow-xl shadow-red-200">Add New</button>
+    </div>
+
+    <div class="flex justify-between rounded-xl bg-white bg-opacity-70 px-4 py-4">
+      <h3>May, 2023</h3>
+      <button class="rounded-full bg-red-400 p-1 shadow-xl shadow-red-400">
+        <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+          <g id="SVGRepo_iconCarrier">
+            <path d="M20 10V7C20 5.89543 19.1046 5 18 5H6C4.89543 5 4 5.89543 4 7V10M20 10V19C20 20.1046 19.1046 21 18 21H6C4.89543 21 4 20.1046 4 19V10M20 10H4M8 3V7M16 3V7" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path>
+            <rect x="6" y="12" width="3" height="3" rx="0.5" fill="#ffffff"></rect>
+            <rect x="10.5" y="12" width="3" height="3" rx="0.5" fill="#ffffff"></rect>
+            <rect x="15" y="12" width="3" height="3" rx="0.5" fill="#ffffff"></rect>
+          </g>
+        </svg>
+      </button>
+    </div>
+
+    <div class="rounded-lg bg-white bg-opacity-70 py-5">
+      <div class="flex items-center justify-center space-x-2 text-center">
+        <div class="rounded-xl bg-white px-3 py-2">
+          <h3>16</h3>
+          <p>Mier</p>
+        </div>
+
+        <div class="rounded-xl bg-white px-3 py-2">
+          <h3>16</h3>
+          <p>Mier</p>
+        </div>
+
+        <div class="rounded-xl bg-blue-600 bg-opacity-60 px-3 py-2 text-white shadow-xl shadow-blue-200">
+          <h3 class="text-lg">16</h3>
+          <p>Mier</p>
+        </div>
+
+        <div class="rounded-xl bg-white px-3 py-2">
+          <h3>16</h3>
+          <p>Mier</p>
+        </div>
+
+        <div class="rounded-xl bg-white px-3 py-2">
+          <h3>16</h3>
+          <p>Mier</p>
+        </div>
+      </div>
+
+      <div class="space-y-2 px-2 text-white">
+        <div class="flex items-center space-x-2 border-b-2 py-4">
+          <h3 class="w-24 text-center text-gray-600">7:00 am</h3>
+          <div class="flex w-full items-center justify-between rounded-xl bg-teal-400 bg-opacity-50 px-4 py-2">
+            <div>
+              <p>Go to supermarket</p>
+              <p>7:00 am - 8:00 am</p>
+            </div>
+            <label class="relative inline-flex cursor-pointer">
+              <input type="checkbox" value="" class="peer sr-only" checked />
+              <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-teal-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-teal-800"></div>
+            </label>
+          </div>
+        </div>
+
+        <div class="flex items-center space-x-2 border-b-2 py-4">
+          <h3 class="w-24 text-center text-gray-600">7:00 am</h3>
+          <div class="flex w-full items-center justify-between rounded-xl bg-teal-400 bg-opacity-50 px-4 py-2">
+            <div>
+              <p>Go to supermarket</p>
+              <p>7:00 am - 8:00 am</p>
+            </div>
+            <label class="relative inline-flex cursor-pointer">
+              <input type="checkbox" value="" class="peer sr-only" checked />
+              <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-teal-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-teal-800"></div>
+            </label>
+          </div>
+        </div>
+
+        <div class="flex items-center space-x-2 border-b-2 py-4">
+          <h3 class="w-24 text-center text-gray-600">7:00 am</h3>
+          <div class="flex w-full items-center justify-between rounded-xl bg-teal-400 bg-opacity-50 px-4 py-2">
+            <div>
+              <p>Go to supermarket</p>
+              <p>7:00 am - 8:00 am</p>
+            </div>
+            <label class="relative inline-flex cursor-pointer">
+              <input type="checkbox" value="" class="peer sr-only" checked />
+              <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-teal-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-teal-800"></div>
+            </label>
+          </div>
+        </div>
+
+        <div class="flex items-center space-x-2 border-b-2 py-4">
+          <h3 class="w-24 text-center text-gray-600">7:00 am</h3>
+          <div class="flex w-full items-center justify-between rounded-xl bg-teal-400 bg-opacity-50 px-4 py-2">
+            <div>
+              <p>Go to supermarket</p>
+              <p>7:00 am - 8:00 am</p>
+            </div>
+            <label class="relative inline-flex cursor-pointer">
+              <input type="checkbox" value="" class="peer sr-only" checked />
+              <div class="peer h-6 w-11 rounded-full bg-gray-200 after:absolute after:left-[2px] after:top-0.5 after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-teal-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-blue-300 dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-teal-800"></div>
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
